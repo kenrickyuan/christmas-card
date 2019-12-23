7bc401b6e78af36d7c2f34a8e9bf2137a18ebf15
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-23 21:23:20 +0100

    renaming model & adding column

--- db/migrate/20191223202239_create_gifts.rb
+++ db/migrate/20191223202239_create_gifts.rb
@@ -1,8 +1,9 @@
-class CreateProducts < ActiveRecord::Migration[5.2]
+class CreateGifts < ActiveRecord::Migration[5.2]
   def change
-    create_table :products do |t|
+    create_table :gifts do |t|
       t.string :name
       t.boolean :choice
+      t.boolean :chosen
       t.integer :number
       t.string :description
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Reset all gifts
+Gift.destroy_all
+
+# Create Gifts
+Gift.create!(
+  name: 'Something for LA and beyond',
+  choice: true,
+  number: 1,
+  description: "Something you can use immediately for our trip to LA. But rest assured, it will help brighten up every journey..."
+  )
+
+Gift.create!(
+  name: 'Something for LA and beyond',
+  choice: true,
+  number: 1,
+  description: "Something you can use immediately for our trip to LA. But rest assured, it will help brighten up every journey..."
+  )

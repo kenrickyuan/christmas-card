fc2b2fcc7e558ce71e2611202bad5200c8786958
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-24 12:48:35 +0100

    added fake/dummy card page + corresponding passcode

--- app/javascript/plugins/password1.js
+++ app/javascript/plugins/password1.js
@@ -1,11 +1,14 @@
 const textField = document.querySelector('#password1');
 const homeButton = document.querySelector('#home-button');
+const fakeButton = document.querySelector('#fake-button');
 
 const password1 = () => {
   textField.addEventListener('keyup', (event) => {
     console.log(event.currentTarget.value);
     if (event.currentTarget.value == "8343") {
       homeButton.classList.remove("hidden");
+    } else if (event.currentTarget.value == "fake") {
+      fakeButton.classList.remove("hidden");
     }
   });
 };
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get '/8343', to: 'pages#card'
+  get '/fake', to: 'pages#fake'
   get '/end', to: 'pages#end'
   resources :gifts, only: [ :index, :show ]
   resources :numbers, only: [ :show ]

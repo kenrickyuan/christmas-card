7d46cdf6857c56a10ffb9e2e82b8beab9ad3d3a8
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-24 03:14:53 +0100

    gift reset logic implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def end
+    reset_gifts
+  end
+
+  private
+
+  def reset_gifts
+    gifts = Gift.all
+    gifts.each do |gift|
+      gift.chosen = false
+      gift.save!
+    end
+  end
 end
